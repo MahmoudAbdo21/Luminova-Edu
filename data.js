a859ff2628aa97d10643f3f01c41cd1f6ac676e9
MahmoudAbdo21 <mahmoud21abdalrhman@gmail.com>
2026-04-04 16:07:03 +0200

    Add files via upload

--- data.js
+++ data.js
@@ -3,7 +3,47 @@ window.LUMINOVA_DATA = {
     "language": "ar",
     "theme": "dark"
   },
-  "news": [],
+  "news": [
+    {
+      "id": "news_1775311312001",
+      "timestamp": "2026-04-04T14:01:52.001Z",
+      "titleAr": "امتحان على الفصل الثالث والرابع مادة الاذاعة والتسجيل ",
+      "titleEn": "Exam on Chapters 3 and 4, Radio and Recording subject",
+      "contentAr": "الامتحان الشامل يضم الفصل الثالث والرابع على جوجل فورم",
+      "contentEn": "The comprehensive exam includes chapters three and four on Google Forms.",
+      "mediaUrl": "",
+      "studentId": "students_1775125739063",
+      "mediaUrls": [
+        "https://docs.google.com/forms/d/e/1FAIpQLScjdrGnOoFatW3u1EE29W5GYIJdr-gKN9dmdTeKiunsDhlHoA/viewform?usp=header"
+      ]
+    },
+    {
+      "id": "news_1775311216667",
+      "timestamp": "2026-04-04T14:00:16.667Z",
+      "titleAr": "امتحان على الفصل الاول و الثاني مادة الاذاعة والتسجيل ",
+      "titleEn": "Exam on the first and second semesters of the Radio and Recording course",
+      "contentAr": "الامتحان الشامل يضم الفصل الاول والفصل الثاني على جوجل فورم",
+      "contentEn": "The comprehensive exam includes both the first and second semesters and is available on Google Forms.",
+      "mediaUrl": "",
+      "studentId": "students_1775125739063",
+      "mediaUrls": [
+        "https://docs.google.com/forms/d/e/1FAIpQLSeN3FPNIeW76pOpVbfsFi4qQRTmQlEmaD88pZUId-0E31IYdQ/viewform?usp=header"
+      ]
+    },
+    {
+      "id": "news_1775309634836",
+      "timestamp": "2026-04-04T13:33:54.836Z",
+      "titleAr": "امتحان على الفصل الاول مادة الاذاعة والتسجيل ",
+      "titleEn": "Exam on the first semester, Radio and Recording course",
+      "contentAr": "الامتحان الشامل يضم الفصل الاول والفصل الثاني على جوجل فورم",
+      "contentEn": "The comprehensive exam includes both the first and second semesters and is available on Google Forms.",
+      "mediaUrl": "",
+      "studentId": "students_1775125739063",
+      "mediaUrls": [
+        "https://docs.google.com/forms/d/e/1FAIpQLSeN3FPNIeW76pOpVbfsFi4qQRTmQlEmaD88pZUId-0E31IYdQ/viewform?usp=header"
+      ]
+    }
+  ],
   "students": [
     {
       "id": "students_1775125739063",
@@ -79,6 +119,63 @@ window.LUMINOVA_DATA = {
   ],
   "summaries": [
     {
+      "id": "summaries_1775310101236",
+      "timestamp": "2026-04-04T13:41:41.236Z",
+      "titleAr": "​أشكال البرامج الإذاعية Level 04",
+      "titleEn": "Radio Program Formats Level 04",
+      "contentAr": "يستعرض هذا الفصل \"هندسة البرامج\"، حيث يفرق بين القوالب الإذاعية المتعددة (الحديث، الحوار، الريبورتاج، الندوة، المجلة، والدراما). يوضح النص المعايير الفنية لنجاح كل قالب، مثل كيفية إدارة الحوار دون \"أسئلة مغلقة\"، والفرق بين التحقيق الحي والمسجل، وكيفية بناء \"فيتشر\" وثائقي عميق. كما يغوص في عالم الدراما الإذاعية والقصة الموجهة للطفل، شارحاً عناصر بناء الشخصية (الجسمية، الاجتماعية، والنفسية) ودور الموسيقى والمؤثرات في خلق واقع افتراضي للأذن",
+      "contentEn": "This chapter explores \"programming,\" distinguishing between various radio formats (talk, dialogue, reportage, panel discussion, magazine, and drama). The text outlines the technical criteria for success in each format, such as how to manage dialogue without \"closed-ended questions,\" the difference between live and recorded investigations, and how to construct an in-depth documentary feature. It also delves into the world of radio drama and storytelling for children, explaining the elements of character development (physical, social, and psychological) and the role of music and sound effects in creating an auditory experience.",
+      "mediaUrl": "",
+      "subjectId": "subjects_1775125681706",
+      "studentId": "students_1775125739063",
+      "yearId": "year_1",
+      "semesterId": "semesters_1775125622791",
+      "mediaUrls": [
+        "file-html/index (4).html",
+        "https://drive.google.com/file/d/1PdRfyy0apwsbg5lQXHok5fe0ySmq9D5Y/view?usp=drivesdk",
+        "file-html/NotebookLM Mind Map (15).png",
+        "file-html/unnamed (5).png"
+      ]
+    },
+    {
+      "id": "summaries_1775309739493",
+      "timestamp": "2026-04-04T13:35:39.493Z",
+      "titleAr": "فن كتابة الأسكريبت الإذاعي Level04",
+      "titleEn": "The Art of Radio Scriptwriting Level 4",
+      "contentAr": "يركز هذا الفصل على الجانب الإبداعي والفني في الإنتاج الإذاعي، وهو \"الكتابة للأذن\". يتناول الفرق بين المسميات الوظيفية للكتاب (مؤلف، سيناريست، معد، محرر)، ويشرح المواصفات الواجب توافرها في المعد الناجح. كما يقدم دليلاً عملياً لكتابة عناصر السكريبت من مقدمات متنوعة، وصياغة أسئلة احترافية، ونهايات مؤثرة، مع توضيح الأسس الهيكلية لبناء البرنامج مثل (وحدة الموضوع، سرعة الإيقاع، والتنوع) لضمان جذب المستمع وتحقيق \"الأصالة\" في المحتوى",
+      "contentEn": "This chapter focuses on the creative and artistic aspects of radio production, namely \"writing for the ear.\" It addresses the differences between the job titles of writers (author, scriptwriter, producer, editor) and explains the qualities a successful producer should possess. It also provides a practical guide to writing script elements, from varied introductions and professionally crafted questions to impactful endings, while clarifying the structural foundations of program development, such as unity of theme, pacing, and variety, to ensure listener engagement and achieve \"authenticity\" in the content.",
+      "mediaUrl": "",
+      "subjectId": "subjects_1775125681706",
+      "studentId": "students_1775125739063",
+      "yearId": "year_1",
+      "semesterId": "semesters_1775125622791",
+      "mediaUrls": [
+        "file-html/index (3).html",
+        "https://drive.google.com/file/d/17f5ZxYTgvgSMqEZgpuEsyOm3dM1XdnoT/view?usp=drivesdk",
+        "file-html/unnamed (4).png",
+        "file-html/NotebookLM Mind Map (14).png"
+      ]
+    },
+    {
+      "id": "summaries_1775307648915",
+      "timestamp": "2026-04-04T13:00:48.915Z",
+      "titleAr": "دليل الإنتاج الإذاعي Level 03",
+      "titleEn": "Radio Production Guide Level 03",
+      "contentAr": "يستعرض هذا الفصل الرحلة المتكاملة لإنتاج العمل الإذاعي، بدءاً من تحديد الجمهور المستهدف كأولوية قصوى تفرز نوع اللغة والوقت والقالب المناسب. ينتقل النص لشرح كيفية اشتقاق الأفكار من الحياة اليومية والمناسبات، ثم خطوات الإعداد التي تشمل التخطيط وجمع المادة من المصادر الشخصية والمكتبية. كما يفصل الفصل مرحلة التنفيذ الفنية (كتابة السكريبت، المونتاج، والمزج الصوتي) وصولاً إلى أدوات تقييم البرنامج لضمان تحقيق أهدافه في تنمية الوعي أو تغيير الاتجاهات والسلوك لدى المستمعين\nشرح الفصل الثاني مراحل الإنتاج الإذاعي",
+      "contentEn": "This chapter explores the complete journey of radio production, beginning with identifying the target audience as a top priority that dictates the appropriate language, timing, and format. The text then explains how to derive ideas from daily life and events, followed by the preparation steps, which include planning and gathering material from personal and office sources. The chapter also details the technical execution phase (scriptwriting, editing, and sound mixing) and concludes with program evaluation tools to ensure the program achieves its objectives of raising awareness or changing listeners' attitudes and behavior.\n\nChapter Two: Stages of Radio Production",
+      "mediaUrl": "",
+      "subjectId": "subjects_1775125681706",
+      "studentId": "students_1775125739063",
+      "yearId": "year_1",
+      "semesterId": "semesters_1775125622791",
+      "mediaUrls": [
+        "file-html/index (2).html",
+        "https://drive.google.com/file/d/149GyGpq3hNvSv4g3ToYFORGATRp-JDkI/view?usp=drivesdk",
+        "file-html/NotebookLM Mind Map (13).png",
+        "file-html/unnamed (3).png"
+      ]
+    },
+    {
       "id": "summaries_1775306210012",
       "timestamp": "2026-04-04T12:36:50.012Z",
       "titleAr": "الاستوديو الإذاعي وأدوات الإنتاج الصوتي Level 03",
@@ -98,7 +195,47 @@ window.LUMINOVA_DATA = {
       ]
     }
   ],
-  "quizzes": [],
+  "quizzes": [
+    {
+      "id": "quizzes_1775311492411",
+      "timestamp": "2026-04-04T14:04:52.411Z",
+      "titleAr": "امتحان على الفصل الثالث والرابع مادة الاذاعة والتسجيل ",
+      "titleEn": "Exam on Chapters 3 and 4, Radio and Recording subject",
+      "isShuffled": false,
+      "feedbackMode": "end",
+      "subjectId": "subjects_1775125681706",
+      "publisherId": "s_founder_hardcoded",
+      "questions": [],
+      "yearId": "year_1",
+      "semesterId": "semesters_1775125622791"
+    },
+    {
+      "id": "quizzes_1775311450759",
+      "timestamp": "2026-04-04T14:04:10.759Z",
+      "titleAr": "امتحان على الفصل الاول و الثاني مادة الاذاعة والتسجيل ",
+      "titleEn": "Exam on the first and second semesters of the Radio and Recording course",
+      "isShuffled": true,
+      "feedbackMode": "end",
+      "subjectId": "subjects_1775125681706",
+      "publisherId": "s_founder_hardcoded",
+      "questions": [],
+      "yearId": "year_1",
+      "semesterId": "semesters_1775125622791"
+    },
+    {
+      "id": "quizzes_1775311390334",
+      "timestamp": "2026-04-04T14:03:10.334Z",
+      "titleAr": "امتحان على الفصل الاول مادة الاذاعة والتسجيل ",
+      "titleEn": "Exam on the first semester, Radio and Recording course",
+      "isShuffled": true,
+      "feedbackMode": "end",
+      "subjectId": "subjects_1775125681706",
+      "publisherId": "s_founder_hardcoded",
+      "questions": [],
+      "yearId": "year_1",
+      "semesterId": "semesters_1775125622791"
+    }
+  ],
   "projects": [
     {
       "id": 1,
