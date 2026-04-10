6b5875dcdc685d80bdbaa226a7716f643e20d37e
MahmoudAbdo21 <mahmoud21abdalrhman@gmail.com>
2026-04-10 15:20:26 +0200

    Add files via upload

--- data.js
+++ data.js
@@ -437,6 +437,102 @@ window.LUMINOVA_DATA = {
   ],
   "summaries": [
     {
+      "id": "summaries_1775826790183",
+      "timestamp": "2026-04-10T13:13:10.183Z",
+      "titleAr": "الفصل الثامن:مشكلات النمو: طفولة مرنة ومراهقة حرجة Level 9",
+      "titleEn": "Chapter 8: Developmental Challenges: Resilient Childhood and Critical Adolescence (Level 9)",
+      "contentAr": "يدرس هذا الفصل الاضطرابات السلوكية (كالعدوان، الكذب، والهروب) التي تعيق توافق الطفل والمراهق. يركز على تحليل الأسباب (سواء كانت عضوية، نفسية، أو مدرسية) ويقدم حلولاً تربوية عملية تعتمد على التدخل المبكر والتعزيز الإيجابي بدلاً من العقاب، بهدف بناء شخصية سوية ومنتجة",
+      "contentEn": "This chapter examines behavioral disorders (such as aggression, lying, and running away) that hinder the adjustment of children and adolescents. It focuses on analyzing the causes (whether organic, psychological, or school-related) and offers practical educational solutions based on early intervention and positive reinforcement rather than punishment, with the aim of building a healthy and productive personality.",
+      "mediaUrl": "",
+      "subjectId": "subjects_1775590782356",
+      "studentId": "students_1775125739063",
+      "yearId": "year_1",
+      "semesterId": "semesters_1775125622791",
+      "mediaUrls": [
+        "https://docs.google.com/forms/d/e/1FAIpQLScbp2wFrM8v5rT6eumOoIQmUAEOBrh28wuKA4kbl-UceDXrzg/viewform?usp=header",
+        "https://drive.google.com/file/d/1fpWH_6eZg7PyYIg_bOFmyhcSwczGJl-R/view?usp=drivesdk",
+        "img/mind map (8).png",
+        "file-html/Developmental psychology (8).html"
+      ]
+    },
+    {
+      "id": "summaries_1775826492929",
+      "timestamp": "2026-04-10T13:08:12.929Z",
+      "titleAr": "من الفصل الرابع حتى السابع دليل رحلة النمو: من الطفولة إلى الرشد Level 8",
+      "titleEn": "Chapters 4-7: A Guide to the Journey of Growth: From Childhood to Adulthood, Level 8",
+      "contentAr": "🚀 محطات رحلة النمو الإنساني\nالمحطة الأولى: عالم الاستكشاف (3–6 سنوات)\nفترة \"الأسئلة الكبرى\" واللعب الخيالي، حيث يبدأ الطفل ببناء لغته واكتشاف محيطه بفضول لا ينتهي.\nالمحطة الثانية: التأسيس والهدوء (6–12 سنة)\nمرحلة المدرسة الابتدائية؛ حيث يهدأ الصخب الجسدي ليحل محله التركيز العقلي، وبناء المهارات الأكاديمية، وتكوين الصداقات\nالمحطة الثالثة: طفرة التحول (12–17 سنة)\nجسر العبور نحو البلوغ، تتسارع فيها التغيرات الجسمانية والنفسية، ويبدأ المراهق برسم ملامح هويته واستقلاليته.\nالمحطة الرابعة: قمة النضج (18–22 سنة)\nمرحلة الشباب والقرار؛ حيث يكتمل البناء الجسدي والعقلي، ويبدأ الفرد في التخطيط لمستقبله المهني والاجتماعي بكل ثبات",
+      "contentEn": "🚀 Stages of Human Growth\n\nStation 1: The World of Exploration (3–6 years)\nA period of \"big questions\" and imaginative play, where children begin to develop their language and explore their surroundings with boundless curiosity.\n\nStation 2: Foundation and Calm (6–12 years)\nElementary school, where physical activity subsides, giving way to mental focus, the development of academic skills, and the formation of friendships.\n\nStation 3: The Transformational Leap (12–17 years)\nThe bridge to adulthood, marked by rapid physical and psychological changes, as adolescents begin to define their identity and independence.\n\nStation 4: Peak Maturity (18–22 years)\nThe stage of youth and decision-making, where physical and mental development is complete, and individuals begin to confidently plan their professional and social futures.",
+      "mediaUrl": "",
+      "subjectId": "subjects_1775590782356",
+      "studentId": "students_1775125739063",
+      "yearId": "year_1",
+      "semesterId": "semesters_1775125622791",
+      "mediaUrls": [
+        "https://drive.google.com/file/d/13pXRfgWnSm0bIvs0C5jSlg_a_mNpvAqk/view?usp=drivesdk",
+        "img/mind map (4-7).png",
+        "img/info psy (4-7).png",
+        "file-html/Developmental psychology(4-7).html"
+      ]
+    },
+    {
+      "id": "summaries_1775826152923",
+      "timestamp": "2026-04-10T13:02:32.923Z",
+      "titleAr": "الفصل الثالث علم نفس النمو",
+      "titleEn": "Chapter Three: Developmental Psychology",
+      "contentAr": "نماذج من نظريات النمو Level7\nفي هذا الفصل، ننتقل من وصف \"ماذا يحدث\" في النمو إلى سبر أغوار \"لماذا وكيف\" يتطور الإنسان، وذلك من خلال عيون أربعة من أعظم المفكرين الذين صاغوا وجدان علم النفس الحديث. يمثل هذا الجزء الخريطة الذهنية والنفسية التي تُفسر السلوك البشري من زوايا متعددة ومتكاملة",
+      "contentEn": "Models of Developmental Theories Level 7\nIn this chapter, we move from describing \"what happens\" in development to exploring \"why and how\" human development, through the eyes of four of the greatest thinkers who shaped the consciousness of modern psychology. This section represents the mental and psychological map that explains human behavior from multiple and integrated perspectives.",
+      "mediaUrl": "",
+      "subjectId": "subjects_1775590782356",
+      "studentId": "students_1775125739063",
+      "yearId": "year_1",
+      "semesterId": "semesters_1775125622791",
+      "mediaUrls": [
+        "https://drive.google.com/file/d/1AqN4QX8v8vkRxicPqTrPrTZooz8JIPsW/view?usp=drivesdk",
+        "file-html/unnamed (7).png",
+        "file-html/NotebookLM Mind Map (19).png",
+        "file-html/index(5).html"
+      ]
+    },
+    {
+      "id": "summaries_1775825859839",
+      "timestamp": "2026-04-10T12:57:39.839Z",
+      "titleAr": "الفصل الثاني :مناهج البحث في علم النفس» LEVEL6",
+      "titleEn": "Chapter Two: Research Methods in Psychology (Level 6)",
+      "contentAr": "ينتقل بنا هذا الجزء من \"ماذا\" ندرس في علم نفس النمو إلى \"كيف\" ندرس ونجمع المعلومات بدقة علمية. يستعرض الفصل الترسانة المنهجية التي يستخدمها الباحثون، بدءاً من المنهج التجريبي الذي يبحث عن العلاقات السببية بين المتغيرات (المستقل والتابع)، وصولاً إلى المنهج الوصفي بكافة أشكاله (المسحي، الارتباطي، والتطوري)",
+      "contentEn": "This section shifts from \"what\" we study in developmental psychology to \"how\" we study and collect information with scientific rigor. The chapter reviews the methodological arsenal used by researchers, from the experimental method, which seeks causal relationships between variables (independent and dependent), to the descriptive method in all its forms (survey, correlational, and developmental)",
+      "mediaUrl": "",
+      "subjectId": "subjects_1775590782356",
+      "studentId": "students_1775125739063",
+      "yearId": "year_1",
+      "semesterId": "semesters_1775125622791",
+      "mediaUrls": [
+        "https://drive.google.com/file/d/1-e8yKmrzr13zST9FwmjpA143IOHRVUav/view?usp=drivesdk",
+        "img/mind map(2).png",
+        "img/info psy (2).png",
+        "file-html/Developmental psychology(2).html"
+      ]
+    },
+    {
+      "id": "summaries_1775825568096",
+      "timestamp": "2026-04-10T12:52:48.096Z",
+      "titleAr": "الفصل الاول: مدخل الى علم نفس النمو» LEVEL6",
+      "titleEn": "Chapter 1: Introduction to Developmental Psychology LEVEL6",
+      "contentAr": "يُعد هذا الفصل دليلاً شاملاً لفهم التغيرات التي تطرأ على الإنسان منذ لحظة التكوين وحتى الشيخوخة. يبدأ بتعريف علم نفس النمو وأهدافه التي تسعى لوصف وتفسير السلوك البشري، موضحاً الفرق الجوهري بين النمو، النضج، والتعلم.\nيستعرض الفصل خريطة متكاملة لمظاهر النمو (الجسمي، العقلي، الانفعالي، والاجتماعي)، ويسلط الضوء على محركات النمو الأساسية مثل الوراثة، والغذاء، والدور الحيوي للجهاز الغدي (كالغدة النخامية والدرقية). كما يقسم حياة الإنسان إلى تسع مراحل زمنية لكل منها \"مطالب نمو\" خاصة وتحديات فريدة. وينتهي الفصل بصياغة القوانين الحاكمة للنمو، مؤكداً أن الإنسان ينمو بنظام دقيق يتجه من العام إلى الخاص ومن الرأس إلى القدم، مما يجعل من فهم هذه الرحلة ضرورة تربوية واجتماعية لبناء شخصية متوازنة",
+      "contentEn": "This chapter serves as a comprehensive guide to understanding the changes that occur in a person from conception to old age. It begins by defining developmental psychology and its aims, which seek to describe and explain human behavior, clarifying the fundamental difference between growth, maturation, and learning.\n\nThe chapter presents a complete map of the aspects of growth (physical, mental, emotional, and social), highlighting the primary drivers of growth such as heredity, nutrition, and the vital role of the endocrine system (such as the pituitary and thyroid glands). It also divides human life into nine time periods, each with its own specific \"growth demands\" and unique challenges. The chapter concludes by formulating the laws governing growth, emphasizing that a person grows in a precise system that progresses from the general to the specific and from head to toe, making understanding this journey an educational and social necessity for building a balanced personality.",
+      "mediaUrl": "",
+      "subjectId": "subjects_1775590782356",
+      "studentId": "students_1775125739063",
+      "yearId": "year_1",
+      "semesterId": "semesters_1775125622791",
+      "mediaUrls": [
+        "https://drive.google.com/file/d/1a2hu2BaH8JGJERpADmPyGzytiyJLg_zL/view?usp=drivesdk",
+        "https://docs.google.com/forms/d/e/1FAIpQLScvQBn5p5NZntgBcl4gSvoZ6zxHRyM4_R2iDI8NxufjZju5Iw/viewform?usp=publish-editor",
+        "img/mind map(1).png",
+        "img/info psy (1).png",
+        "file-html/Developmental psychology(1).html"
+      ]
+    },
+    {
       "id": "summaries_1775824664181",
       "timestamp": "2026-04-10T12:37:44.181Z",
       "titleAr": "سكشن الأسس النظرية الي الدكتوره طلبت نعمل موقع علي google site",
@@ -466,25 +562,6 @@ window.LUMINOVA_DATA = {
       "semesterId": "semesters_1775125622791",
       "mediaUrls": [
         "https://drive.google.com/file/d/17hBP-NY9a5ei7tvebdtizZtEJTt0g76t/view?usp=sharing"
-      ]
-    },
-    {
-      "id": "summaries_1775590829401",
-      "timestamp": "2026-04-07T19:40:29.401Z",
-      "titleAr": "الفصل الثالث علم نفس النمو",
-      "titleEn": "Chapter Three: Developmental Psychology",
-      "contentAr": "نماذج من نظريات النمو Level7\nفي هذا الفصل، ننتقل من وصف \"ماذا يحدث\" في النمو إلى سبر أغوار \"لماذا وكيف\" يتطور الإنسان، وذلك من خلال عيون أربعة من أعظم المفكرين الذين صاغوا وجدان علم النفس الحديث. يمثل هذا الجزء الخريطة الذهنية والنفسية التي تُفسر السلوك البشري من زوايا متعددة ومتكاملة",
-      "contentEn": "Models of Developmental Theories Level 7\nIn this chapter, we move from describing \"what happens\" in development to exploring \"why and how\" human development, through the eyes of four of the greatest thinkers who shaped the consciousness of modern psychology. This section represents the mental and psychological map that explains human behavior from multiple and integrated perspectives.",
-      "mediaUrl": "",
-      "subjectId": "subjects_1775590782356",
-      "studentId": "students_1775125739063",
-      "yearId": "year_1",
-      "semesterId": "semesters_1775125622791",
-      "mediaUrls": [
-        "https://drive.google.com/file/d/1AqN4QX8v8vkRxicPqTrPrTZooz8JIPsW/view?usp=drivesdk",
-        "file-html/unnamed (7).png",
-        "file-html/NotebookLM Mind Map (19).png",
-        "file-html/index(5).html"
       ]
     },
     {
