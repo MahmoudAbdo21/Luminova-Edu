cf7244ab34db31d99c3af9d9def4704741bf3af5
MahmoudAbdo21 <mahmoud21abdalrhman@gmail.com>
2026-04-15 20:10:00 +0200

    Add files via upload

--- certificates.js
+++ certificates.js
@@ -1,1 +1,20 @@
-window.LUMINOVA_CERTIFICATES = [];
+window.LUMINOVA_CERTIFICATES = [
+  {
+    "id": "certificates_1776275832139",
+    "timestamp": "2026-04-15T17:57:12.139Z",
+    "studentName": "عمر عبدالحكيم ",
+    "studentNameEn": "Omar Abdul Hakeem",
+    "senderName": "سلمى زهران",
+    "senderNameEn": "Salma Zahran",
+    "senderRole": "Level Up",
+    "title": "شهادة تقدير تميز أكاديمي",
+    "titleEn": "Certificate of Academic Excellence",
+    "description": "لتفوقه الملحوظ وحصوله على أعلى الدرجات في التقييمات الأكاديمية.",
+    "descriptionEn": "For his remarkable excellence and highest scores in academic assessments.",
+    "isFeatured": true,
+    "badges": [],
+    "date": "2026-04-15T17:57:12.139Z",
+    "senderRoleEn": "Level up",
+    "sealType": "silver"
+  }
+];
