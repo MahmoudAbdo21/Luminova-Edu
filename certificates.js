2f75ec92d5b7d459e09c03fc5c43908b8d96ad01
MahmoudAbdo21 <mahmoud21abdalrhman@gmail.com>
2026-05-06 16:14:33 +0300

    Add files via upload

--- certificates.js
+++ certificates.js
@@ -1,5 +1,23 @@
 window.LUMINOVA_CERTIFICATES = [
   {
+    "id": "certificates_1778072930957",
+    "timestamp": "2026-05-06T13:08:50.957Z",
+    "studentName": "ملك طلبه عبدالعزيز",
+    "studentNameEn": "Malak Tolba Abdalazez",
+    "senderName": "فريق Level Up",
+    "senderNameEn": "Level up",
+    "senderRole": "Level Up",
+    "title": "شهادة تقدير تميز أكاديمي",
+    "titleEn": "Certificate of Academic Excellence",
+    "description": "لتفوقه الملحوظ وحصوله على أعلى الدرجات في التقييمات الأكاديمية.",
+    "descriptionEn": "For his remarkable excellence and highest scores in academic assessments.",
+    "isFeatured": true,
+    "badges": [],
+    "date": "2026-05-06T13:08:50.957Z",
+    "sealType": "silver",
+    "senderRoleEn": "Level up"
+  },
+  {
     "id": "certificates_1776277229935",
     "timestamp": "2026-04-15T18:20:29.935Z",
     "studentName": "عمر عبدالحكيم عبدالمحسن",
@@ -11,7 +29,7 @@ window.LUMINOVA_CERTIFICATES = [
     "titleEn": "Certificate of Academic Excellence",
     "description": "لتفوقه الملحوظ وحصوله على أعلى الدرجات في التقييمات الأكاديمية.",
     "descriptionEn": "For his remarkable excellence and highest scores in academic assessments.",
-    "isFeatured": true,
+    "isFeatured": false,
     "badges": [],
     "date": "2026-04-15T18:20:29.935Z",
     "sealType": "silver",
